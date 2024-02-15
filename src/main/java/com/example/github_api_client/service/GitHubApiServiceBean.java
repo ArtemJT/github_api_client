1358315e5adef44977b6d80757d76add4a749551
Artem <105941754+ArtemJT@users.noreply.github.com>
2024-02-15 23:40:26 +0200

    All logic moved into GitHubRepoMapper class

--- src/main/java/com/example/github_api_client/service/GitHubApiServiceBean.java
+++ src/main/java/com/example/github_api_client/service/GitHubApiServiceBean.java
@@ -1,19 +1,15 @@
 package com.example.github_api_client.service;
 
-import com.example.github_api_client.dto.GitHubBranchDto;
 import com.example.github_api_client.dto.GitHubRepositoryDto;
+import com.example.github_api_client.mapper.GitHubRepoMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.kohsuke.github.GHBranch;
-import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GHUser;
 import org.kohsuke.github.GitHub;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 /**
  * @author Artem Kovalov on 13.02.2024
@@ -27,34 +23,12 @@ public class GitHubApiServiceBean implements GitHubApiService {
 
     @Override
     public GHUser getGHUser(String userName) throws IOException {
-        GHUser user = gitHub.getUser(userName);
-        log.info("USER LOGIN: " + user.getLogin());
-        return user;
+        return gitHub.getUser(userName);
     }
 
     @Override
     public List<GitHubRepositoryDto> gitHubRepos(String userName) throws IOException {
-        GHUser ghUser = getGHUser(userName);
-        Map<String, GHRepository> repositories = ghUser.getRepositories();
-        List<GitHubRepositoryDto> gitHubRepositoryDtoList = new ArrayList<>();
-        for (GHRepository repository : repositories.values()) {
-            List<GitHubBranchDto> branchList = new ArrayList<>();
-            for (GHBranch branch : repository.getBranches().values()) {
-                var repoBranch = GitHubBranchDto.builder()
-                        .branchName(branch.getName())
-                        .commitSha1(branch.getSHA1())
-                        .build();
-                branchList.add(repoBranch);
-            }
-            var repo = GitHubRepositoryDto.builder()
-                    .repoName(repository.getName())
-                    .ownerLogin(repository.getOwnerName())
-                    .repoBranches(branchList)
-                    .isFork(repository.isFork())
-                    .build();
-            gitHubRepositoryDtoList.add(repo);
-        }
-        return gitHubRepositoryDtoList;
+        return GitHubRepoMapper.makeGitHubRepositoryDtoList(getGHUser(userName).getRepositories());
     }
 
     @Override
