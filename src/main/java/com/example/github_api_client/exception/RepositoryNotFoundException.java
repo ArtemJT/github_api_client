41bf6238e08334c35d0245f6f82cc75969743e0d
Artem <105941754+ArtemJT@users.noreply.github.com>
2024-02-15 23:35:24 +0200

    Custom exception

---
+++ src/main/java/com/example/github_api_client/exception/RepositoryNotFoundException.java
@@ -0,0 +1,11 @@
+package com.example.github_api_client.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * @author Artem Kovalov on 15.02.2024
+ */
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class RepositoryNotFoundException extends RuntimeException {
+}
