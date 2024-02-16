8ba0c98f1679710f51effcb00d4ad187b562e13d
Artem <105941754+ArtemJT@users.noreply.github.com>
2024-02-16 22:50:28 +0200

    Added Exception Handler

--- src/main/java/com/example/github_api_client/exception/GitHubApiGlobalExceptionHandler.java
+++ src/main/java/com/example/github_api_client/exception/GitHubApiGlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.example.github_api_client.exception;
 
-import org.kohsuke.github.GHFileNotFoundException;
+import com.example.github_api_client.exception.custom_exceptions.GHNotForkRepositoryNotFoundException;
+import com.example.github_api_client.exception.custom_exceptions.GHRepositoryBranchNotFoundException;
+import com.example.github_api_client.exception.custom_exceptions.GHRepositoryNotFoundException;
+import com.example.github_api_client.exception.custom_exceptions.GHUserNotFoundException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +11,7 @@ import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 
 /**
@@ -16,9 +20,9 @@ import org.springframework.web.context.request.WebRequest;
 @ControllerAdvice
 public class GitHubApiGlobalExceptionHandler {
 
-    @Value("${templates.exception.gHFileNotFound.mustBeNotEmpty}")
+    @Value("${templates.exception.mustBeNotEmpty}")
     private String mustNotBeEmpty;
-    @Value("${templates.exception.gHFileNotFound.userNotFound}")
+    @Value("${templates.exception.userNotFound}")
     private String notFound;
     @Value("${templates.exception.mediaTypeNotAccept}")
     private String acceptResponse;
@@ -26,9 +30,12 @@ public class GitHubApiGlobalExceptionHandler {
     private String repositoryNotFound;
     @Value("${templates.exception.notForkRepositoryNotFound}")
     private String notForkRepositoryNotFound;
+    @Value("${templates.exception.branchNotFound}")
+    private String branchNotFound;
 
-    @ExceptionHandler(GHFileNotFoundException.class)
-    public ResponseEntity<ErrorDetails> ghFileNotFoundException(WebRequest request) {
+    @ExceptionHandler(GHUserNotFoundException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    public ResponseEntity<ErrorDetails> ghUserNotFoundException(WebRequest request) {
         String requestUserName = request.getParameter("userName");
         notFound += requestUserName;
         String response = (requestUserName == null || requestUserName.isBlank()) ? mustNotBeEmpty : notFound;
@@ -36,21 +43,31 @@ public class GitHubApiGlobalExceptionHandler {
     }
 
     @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
+    @ResponseStatus(value = HttpStatus.NOT_ACCEPTABLE)
     public ResponseEntity<ErrorDetails> httpMediaTypeException() {
         return ErrorDetails.getResponseEntity(acceptResponse, HttpStatus.NOT_ACCEPTABLE);
     }
 
-    @ExceptionHandler(RepositoryNotFoundException.class)
+    @ExceptionHandler(GHRepositoryNotFoundException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
     public ResponseEntity<ErrorDetails> repositoryNotFoundException() {
         return ErrorDetails.getResponseEntity(repositoryNotFound, HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(NotForkRepositoryNotFoundException.class)
+    @ExceptionHandler(GHRepositoryBranchNotFoundException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    public ResponseEntity<ErrorDetails> repositoryBranchNotFoundException() {
+        return ErrorDetails.getResponseEntity(branchNotFound, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(GHNotForkRepositoryNotFoundException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
     public ResponseEntity<ErrorDetails> notForkRepositoryNotFoundException() {
         return ErrorDetails.getResponseEntity(notForkRepositoryNotFound, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(MissingServletRequestParameterException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     public ResponseEntity<ErrorDetails> missingServletRequestParameterException(Exception ex) {
         return ErrorDetails.getResponseEntity(ex.getLocalizedMessage(), HttpStatus.BAD_REQUEST);
     }
